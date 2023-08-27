2e247d1970c23872759d0bdfce7db71572075aff
broline <brolineoronu3@gmail.com>
2023-08-27 10:29:14 +0300

    makeChange

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+makeChange Algorithm
+"""
+START_DATA = 0
+
+
+def makeChange(coins, total):
+    """ With a pile of coins of different values,
+    return minimum number of coins needed to get a total amount.
+    """
+    if total < 1:
+        return 0
+    coins.sort()
+    coins.reverse()
+    fewest = START_DATA
+    for coin in coins:
+        if total <= 0:
+            break
+        buff = total // coin
+        fewest += buff
+        total -= (buff * coin)
+    if total != 0:
+        return -1
+    return fewest
